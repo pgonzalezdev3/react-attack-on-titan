00d8f1ef8c6c00ae638a18fb715e8d6c9a5edd84
pedrogf84 <dev.pgonzalez@gmail.com>
2024-03-23 08:24:29 +0100

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,30 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Switch,
+} from "react-router-dom";
 import "./App.css";
-import AOTCharacters from "./layouts/AOTCharacters";
+import Header from "./layouts/Header";
+import Home from "./pages/Home";
+import Titanes from "./pages/Titanes";
+import About from "./pages/About";
+import ProtectedRoute from "./layouts/ProtectedRoute";
+import LoginForm from "./pages/LoginForm";
 
 function App() {
   return (
-    <>
-      <AOTCharacters />
-    </>
+    <Router>
+      <Header />
+      <main>
+        <Switch>
+          <Route path="/login" element={<LoginForm />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/titanes" element={<Titanes />} />
+          <Route path="/about" element={<About />} />
+        </Switch>
+      </main>
+    </Router>
   );
 }
 
--- src/layouts/AOTCharacters.jsx
+++ src/layouts/AOTCharacters.jsx
@@ -1,4 +1,4 @@
-import axios from "axios";
+/* import axios from "axios";
 import React, { useEffect, useState } from "react";
 import AOTCharacterCard from "./AOTCharacterCard";
 
@@ -50,6 +50,57 @@ function AOTCharacters() {
         </div>
       </div>
     </div>
+  );
+}
+
+export default AOTCharacters;
+ */
+
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import AOTCharacterCard from "./AOTCharacterCard";
+
+function AOTCharacters() {
+  const [name, setName] = useState("");
+  const [characters, setCharacters] = useState([]);
+  useEffect(() => {
+    const url = `https://api.attackontitanapi.com/characters?name=${name}`;
+    axios.get(url).then((res) => {
+      const filterData = res.data.results.map((character) => ({
+        id: character.id,
+        name: character.name,
+        age: character.age,
+        img: character.img.split(".png")[0] + ".png",
+        gender: character.gender,
+        occupation: character.occupation,
+      }));
+      setCharacters(filterData);
+    });
+  }, [name]);
+
+  return (
+    <>
+      <div>
+        <div className="main">
+          <div className="search">
+            <h1>Personajes de Attack on Titan</h1>
+            <input
+              type="text"
+              onChange={(event) => {
+                setName(event.target.value);
+              }}
+              value={name.toLowerCase()}
+              placeholder="Buscar por nombre"
+            />
+          </div>
+          <div className="card-container">
+            {characters.map((character) => (
+              <AOTCharacterCard key={character.id} data={character} />
+            ))}
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
